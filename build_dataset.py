99ab4c4227318df8aafcc29553898956403d20b9
pavankumarreddy15 <55206955+pavankumarreddy15@users.noreply.github.com>
2021-08-06 22:50:19 +0530

    Update build_dataset.py

--- build_dataset.py
+++ build_dataset.py
@@ -1,7 +1,6 @@
 from torchvision import datasets
 import argparse, os
 import numpy as np
-from numba import jit,cuda
 from submodlib.functions.facilityLocation import FacilityLocationFunction
 
 parser = argparse.ArgumentParser()
@@ -25,7 +24,7 @@ COUNTS = {
 
 _DATA_DIR = "data"
 
-@jit(target="cuda")
+
 def split_l_u(train_set, n_labels, setting):
     # NOTE: this function assume that train_set is shuffled.
     images = train_set["images"]
